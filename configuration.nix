b8eb116aa550966f5c4a53b4ef374535a1fe5bc7
plastic-melter <140357149+plastic-melter@users.noreply.github.com>
2026-01-26 14:18:10 -0800

    flake update: 2026-01-26 14:18:10

--- configuration.nix
+++ configuration.nix
@@ -25,6 +25,7 @@ nix = {
     require-sigs = true;
     trusted-public-keys = [
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
     accept-flake-config = true;
   };
